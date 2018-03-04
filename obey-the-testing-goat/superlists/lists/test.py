9268ab0448f2eb067b341bc3637f0e6de0e8348a
Ratul Minhaz <m@ratul.xyz>
2018-03-04 19:25:28 +0600

    First unit test and url mapping, dummy view

--- obey-the-testing-goat/superlists/lists/test.py
+++ obey-the-testing-goat/superlists/lists/test.py
@@ -1,6 +1,9 @@
 from django.urls import resolve
-from lists.views import home_page
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
+
 
 # Functional test
 class HomePageTest(TestCase):
@@ -8,6 +11,13 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)     # Unit test
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
 # class SmokeTest(TestCase):
 
 #     def test_bad_maths(self):
