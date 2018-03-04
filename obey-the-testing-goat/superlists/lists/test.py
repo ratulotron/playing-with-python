7b5d3c5c78f5fd5f0a36b2d08b40a0eec6198690
Ratul Minhaz <ratul@Pathaos-MacBook-Pro.local>
2018-03-04 18:58:08 +0600

    First unit test and url mapping, dummy view

--- obey-the-testing-goat/superlists/lists/test.py
+++ obey-the-testing-goat/superlists/lists/test.py
@@ -2,10 +2,11 @@ from django.urls import resolve
 from lists.views import home_page
 from django.test import TestCase
 
+# Functional test
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        self.assertEqual(found.func, home_page)
+        self.assertEqual(found.func, home_page)     # Unit test
 
 # class SmokeTest(TestCase):
 
